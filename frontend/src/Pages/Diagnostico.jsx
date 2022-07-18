9043ce6c2ae17e083eb20dcc5b09ce1178233b8f
joaquinrfs <joaquinrfs@tutanota.com>
2022-07-18 05:16:32 -0400

    Mejora diagnostico
    - Corrige ortografia
    - Agrega formulario compra

--- frontend/src/Pages/Diagnostico.jsx
+++ frontend/src/Pages/Diagnostico.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 // El uso de funciones dentro de un mismo .jsx en lugar de .js externo
 // permite al código modificar el DOM virtual generado por React.
@@ -6,7 +7,7 @@ import React from 'react';
 // Las siguientes cuatro funciones se encargan de:
 // 1. UpdateDeviceType: Activa la selección de Marcas según el tipo de dispositivo y
 //    actualiza el detalle.
-// 2. UpdateDeviceBrand: Activa la selección de Categorias (de problemas) según el tipo
+// 2. UpdateDeviceBrand: Activa la selección de Categorías (de problemas) según el tipo
 //    de dispositivo y la marca, y actualiza el detalle.
 // 3. UpdateDeviceCategory: Genera (prototipo) las soluciones al problema y el precio en base al
 //    problema, y actualiza el detalle.
@@ -21,7 +22,7 @@ function UpdateDeviceType() {
     // a esto deshabilita o no la siguiente opción (Marca).
     if (deviceType.value === 'none') {
         deviceBrand.setAttribute('disabled', '');
-        deviceBrand.innerHTML = '<option value="none">Selecciona una opcion...</option>'
+        deviceBrand.innerHTML = '<option value="none">Selecciona una opción...</option>'
         
         detailType.removeAttribute('value');
         detailTypeText.innerHTML = 'N/A';
@@ -35,10 +36,10 @@ function UpdateDeviceType() {
         switch(deviceType.value)
         {
             case 'computador':
-                deviceBrand.innerHTML = '<option value="none">Selecciona una opcion...</option><option value="acer">Acer</option><option value="apple">Apple</option><option value="asus">Asus</option><option value="dell">Dell</option><option value="gear">Gear</option><option value="gigabyte">Gigabyte</option><option value="google">Google</option><option value="hp">HP</option><option value="huawei">Huawei</option><option value="lenovo">Lenovo</option><option value="microsoft">Microsoft</option><option value="msi">MSI</option><option value="samsung">Samsung</option><option value="toshiba">Toshiba</option>';
+                deviceBrand.innerHTML = '<option value="none">Selecciona una opción...</option><option value="acer">Acer</option><option value="apple">Apple</option><option value="asus">Asus</option><option value="dell">Dell</option><option value="gear">Gear</option><option value="gigabyte">Gigabyte</option><option value="google">Google</option><option value="hp">HP</option><option value="huawei">Huawei</option><option value="lenovo">Lenovo</option><option value="microsoft">Microsoft</option><option value="msi">MSI</option><option value="samsung">Samsung</option><option value="toshiba">Toshiba</option>';
                 break;
             case 'telefono':
-                deviceBrand.innerHTML = '<option value="none">Selecciona una opcion...</option><option value="alcatel">Alcatel</option><option value="apple">Apple</option><option value="google">Google</option><option value="huawei">Huawei</option><option value="lg">LG</option><option value="motorola">Motorola</option><option value="nokia">Nokia</option><option value="oneplus">OnePlus</option><option value="oppo">Oppo</option><option value="samsung">Samsung</option><option value="Sony">Sony</option><option value="vivo">Vivo</option><option value="Xiaomi">Xiaomi</option><option value="zte">ZTE</option>';
+                deviceBrand.innerHTML = '<option value="none">Selecciona una opción...</option><option value="alcatel">Alcatel</option><option value="apple">Apple</option><option value="google">Google</option><option value="huawei">Huawei</option><option value="lg">LG</option><option value="motorola">Motorola</option><option value="nokia">Nokia</option><option value="oneplus">OnePlus</option><option value="oppo">Oppo</option><option value="samsung">Samsung</option><option value="Sony">Sony</option><option value="vivo">Vivo</option><option value="Xiaomi">Xiaomi</option><option value="zte">ZTE</option>';
                 break;
         }
     }
@@ -52,15 +53,15 @@ function UpdateDeviceBrand() {
     var detailBrand = document.getElementById('detailBrand');
     var detailBrandText = document.getElementById('detailBrandText');
 
-    var issuesBase = '<optgroup label="Problemas Generales"><option value="none">Selecciona una opcion...</option><option value="audio">Audio</option><option value="bateria">Bateria</option><option value="encendido">Encendido</option><option value="internet">Internet</option><option value="pantalla">Pantalla</option><option value="velocidad">Velocidad</option></optgroup>';
+    var issuesBase = '<optgroup label="Problemas Generales"><option value="none">Selecciona una opción...</option><option value="audio">Audio</option><option value="bateria">Bateria</option><option value="encendido">Encendido</option><option value="internet">Internet</option><option value="pantalla">Pantalla</option><option value="velocidad">Velocidad</option></optgroup>';
     var issuesType;
     var issuesBrand;
 
     // Verifica si el usuario seleccionó una opción en "Marca de Dispositivo" y en base
-    // a esto deshabilita o no la siguiente opción (Categoria de Problema).
+    // a esto deshabilita o no la siguiente opción (Categoría de Problema).
     if (deviceBrand.value === 'none') {
         deviceCategory.setAttribute('disabled', '');
-        deviceCategory.innerHTML = '<option value="none">Selecciona una opcion...</option>'
+        deviceCategory.innerHTML = '<option value="none">Selecciona una opción...</option>'
         
         detailBrand.removeAttribute('value');
         detailBrandText.innerHTML = 'N/A';
@@ -105,7 +106,7 @@ function UpdateDeviceBrand() {
                     break;
             }
         }
-        deviceCategory.innerHTML = issuesBase + '<optgroup label="Problemas Especificos">' + issuesType + issuesBrand + '</optgroup>';
+        deviceCategory.innerHTML = issuesBase + '<optgroup label="Problemas Específicos">' + issuesType + issuesBrand + '</optgroup>';
     }
     UpdateDeviceCategory();
 }
@@ -213,6 +214,92 @@ function UpdateDeviceOther() {
     }
 }
 
+function SweetAlert() {
+    const onSubmit = (e) => {e.preventDefault()}; // Evita que los forms recarguen la pagina.
+    
+    Swal.fire({
+        title: 'Información',
+        html:
+            '<div class="text-start">' +
+                '<div class="d-flex justify-content-between gap-3 flex-wrap mb-3">' +
+                    '<div class="flex-grow-1">' +
+                        '<label for="userFirstName" class="form-label">Nombre:</label>' +
+                        '<input type="text" id="userFirstName" class="form-control" name="userFirstName">' +
+                    '</div>' +
+                    '<div class="flex-grow-1">' +
+                        '<label for="userLastName" class="form-label">Apellido:</label>' +
+                        '<input type="text" id="userLastName" class="form-control" name="userLastName">' +
+                    '</div>' +
+                '</div>' +
+                '<div class="d-flex justify-content-between gap-3 flex-wrap my-3">' +
+                    '<div class="flex-grow-1">' +
+                        '<label for="userCountry" class="form-label">País:</label>' +
+                        '<select id="userCountry" class="form-select" name="userCountry">' +
+                            '<option value="chile">Chile</option>' +
+                        '</select>' +
+                    '</div>' +
+                    '<div>' +
+                        '<label for="userRegion" class="form-label">Región:</label>' +
+                        '<input type="text" id="userRegion" class="form-control" name="userRegion">' +
+                    '</div>' +
+                '</div>' +
+                '<div class="d-flex justify-content-between gap-3 my-3">' +
+                    '<div>' +
+                        '<label for="userDistrict" class="form-label">Comuna:</label>' +
+                        '<input type="text" id="userDistrict" class="form-control" name="userDistrict">' +
+                    '</div>' +
+                    '<div>' +
+                        '<label for="userStreet" class="form-label">Calle:</label>' +
+                        '<input type="text" id="userStreet" class="form-control" name="userStreet">' +
+                    '</div>' +
+                    '<div>' +
+                        '<label for="userNumber" class="form-label">Número:</label>' +
+                        '<input type="text" id="userNumber" class="form-control" name="userNumber">' +
+                    '</div>' +
+                '</div>' +
+                '<div class="my-3">' +
+                    '<label for="userPhone" class="form-label">Teléfono:</label>' +
+                    '<input type="tel" id="userPhone" class="form-control" name="userPhone">' +
+                '</div>' +
+                '<div class="mt-3">' +
+                    '<label for="userEmail" class="form-label">Correo Electrónico:</label>' +
+                    '<input type="email" id="userEmail" class="form-control" name="userEmail">' +
+                '</div>' +
+            '</div>',
+        showCloseButton: false,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText:
+            'Aceptar',
+        cancelButtonText:
+            'Cancelar',
+        preConfirm: () => {
+            return [
+                document.getElementById('userFirstName').value,
+                document.getElementById('userLastName').value,
+                document.getElementById('userCountry').value,
+                document.getElementById('userRegion').value,
+                document.getElementById('userDistrict').value,
+                document.getElementById('userStreet').value,
+                document.getElementById('userNumber').value,
+                document.getElementById('userPhone').value,
+                document.getElementById('userEmail').value
+            ]
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title:
+                    'Petición enviada',
+                text:
+                    'Un ejecutivo se contactara con usted a la brevedad.',
+                confirmButtonText:
+                    'Aceptar'
+            });
+        }
+    });
+}
+
 // Esta función es exportada al Template Engine, el contenido de la pagina
 // va dentro de un elemento '<>' porque la exportación solo soporta un elemento.
 function Diagnostico() {
@@ -220,14 +307,14 @@ function Diagnostico() {
         <>
             <main className="flex-grow-1">
                 <div className="container my-5">
-                    <h1 className="mb-4 pb-3 border-bottom border-dark border-1">Diagnostico, <strong>¡es gratis!</strong></h1>
+                    <h1 className="mb-4 pb-3 border-bottom border-dark border-1">Diagnóstico, <strong>¡es gratis!</strong></h1>
                     <div className="row">
                         <div className="col-8">
                             <form action="" method="get">
                                 <div className="mb-3">
                                     <label htmlFor="deviceType" className="form-label">¿Con que tipo de dispositivo esta teniendo problemas?</label>
                                     <select id="deviceType" className="form-select" name="deviceType" onChange={UpdateDeviceType}>
-                                        <option value="none">Selecciona una opcion...</option>
+                                        <option value="none">Selecciona una opción...</option>
                                         <option value="computador">Computador</option>
                                         <option value="telefono">Teléfono</option>
                                     </select>
@@ -235,13 +322,13 @@ function Diagnostico() {
                                 <div className="my-3">
                                     <label htmlFor="" className="form-label">¿Que marca es el dispositivo?</label>
                                     <select id="deviceBrand" className="form-select" name="deviceBrand" onChange={UpdateDeviceBrand} disabled>
-                                        <option value="none">Selecciona una opcion...</option>
+                                        <option value="none">Selecciona una opción...</option>
                                     </select>
                                 </div>
                                 <div className="my-3">
                                     <label htmlFor="deviceCategory" className="form-label">¿Que tipo de problema tiene?</label>
                                     <select id="deviceCategory" className="form-select" name="deviceCategory" onChange={UpdateDeviceCategory} disabled>
-                                        <option value="none">Selecciona una opcion...</option>
+                                        <option value="none">Selecciona una opción...</option>
                                     </select>
                                 </div>
                                 <div className="my-3">
@@ -276,7 +363,7 @@ function Diagnostico() {
                                     <h4 className="mb-0">Precio estimado:</h4>
                                     <span id="detailPriceText" className="fs-2">$55.500</span>
                                 </div>
-                                <button type="submit" className="btn btn-primary fs-3">¡Contrátanos!</button>
+                                <button type="submit" className="btn btn-primary fs-3" onClick={SweetAlert}>¡Contrátanos!</button>
                             </div>
                         </div>
                     </div>
