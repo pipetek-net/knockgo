ad63fc4c803cad91f29b2d900f63379d1299f925
MrPotatoXx <salinasgiovanni25@gmail.com>
2022-07-17 20:05:59 -0400

    Se arregla el modelo de usuario

--- backend/model/usuario.js
+++ backend/model/usuario.js
@@ -2,6 +2,7 @@ const { model, Schema} = require('mongoose');
 
 const UsuarioSchema = new Schema({
     nombre: {type : String, required : true},
+    apellido: {type : String, required : true},
     correo: {type : String, required : true, unique : true},
     contraseña: {type : String, required : true},
     tipo: {type : Number, required : true, default : 3},
